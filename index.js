2c06226e610b08bcbbafecefa6f9c84128d0d347
Nicolas <nicolasricciardi24@gmail.com>
2022-09-09 20:23:34 -0300

    fix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-let numero1 = (prompt("Su número es par o impar? Ingrese su número o ingrese ESC para terminar el programa"));
+/* let numero1 = (prompt("Su número es par o impar? Ingrese su número o ingrese ESC para terminar el programa"));
 
 while(numero1 != "ESC"){
 
@@ -10,4 +10,30 @@ while(numero1 != "ESC"){
     };
 
     numero1 = (prompt("Ingrese su numero o ingrese ESC para terminar el programa"));
-};
+}; */
+
+
+class Producto{
+    constructor(nombre, imagen, precio, cantidad){
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagen = imagen;
+        this.vendido = false;
+        this.cantidad = cantidad;
+    }
+
+    vender(){
+        if(this.cantidad === 0 ){
+            this.vendido = false;
+        }
+        else{
+            this.cantidad = this.cantidad - 1;
+        }
+    }
+    }
+
+    const producto1 = new Producto("monitor", "htt", 1234, 5);
+    console.log(producto1);
+
+    producto1.vender();
+    console.log(producto1);
