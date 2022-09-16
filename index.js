9926f4740c4a0343fcba634bab9f117480eb5acb
Nicolas <nicolasricciardi24@gmail.com>
2022-09-15 23:35:25 -0300

    Arreglo por el tutor(Optimizacion de codigo)

--- index.js
+++ index.js
@@ -3,27 +3,21 @@ function dolar(numero, tipo){
     switch(tipo){
         case "oficial":
             return numero * 139;
-            break;
         case "blue":
             return numero * 270;
-            break;
         case "liqui":
             return numero * 280;
-            break;
         default :
         console.log("Tipo de dolar no identificado");
         break;
     }
 }
 
-let resultado = dolar();
 let tipoDolar = (prompt("Ingrese el tipo de dolar al que quiere cambiar sus pesos: oficial, blue o liqui (Ingrese ESC para salir del programa)"));
 let pesos = (prompt("Ingrese, sin el simbolo $, la cantidad de pesos a cambiar"));
-let mensaje = (`$${pesos} son $${dolar(pesos, tipoDolar)} USD al cambio ${tipoDolar}`);
 
 while(tipoDolar != "ESC"){
-    alert(mensaje);
+    alert(`$${pesos} son $${dolar(pesos, tipoDolar)} USD al cambio ${tipoDolar}`);
     tipoDolar = prompt("Ingrese el tipo de dolar al que quiere cambiar sus pesos: oficial, blue o liqui (Ingrese ESC para salir del programa)");
     pesos = prompt("Ingrese, sin el simbolo $, la cantidad de pesos a cambiar");
-    mensaje = (`$${pesos} son $${dolar(pesos, tipoDolar)} USD al cambio ${tipoDolar}`);
 };
