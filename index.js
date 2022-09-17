a05b15b2d18a2fba946b0103d099b6a50a81528a
Nicolas <nicolasricciardi24@gmail.com>
2022-09-17 02:17:09 -0300

    Desafio incorporar arrays (Mini base de datos con arrays y objetos)

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
+const nombres = ["oficial", "blue", "liqui"];
+const precios = [139, 270, 280];
+
+function tdolar(nombre, precio){
+    this.nombre = nombre
+    this.precio = precio
+};
+
+const oficial = new tdolar(nombres[0], precios[0]);
+const blue = new tdolar(nombres[1], precios[1]);
+const liqui = new tdolar(nombres[2], precios[2]);
+
 /* Funcion para calcular los dolares */
-function dolar(numero, tipo){
-    switch(tipo){
-        case "oficial":
-            return numero * 139;
-        case "blue":
-            return numero * 270;
-        case "liqui":
-            return numero * 280;
+function dolar(numero, tipoDolar){
+    switch(tipoDolar){
+        case oficial.nombre:
+            return numero * oficial.precio;
+        case blue.nombre:
+            return numero * blue.precio;
+        case liqui.nombre:
+            return numero * liqui.precio;
         default :
         console.log("Tipo de dolar no identificado");
         break;
-    }
-}
+    };
+};
 
-let tipoDolar = (prompt("Ingrese el tipo de dolar al que quiere cambiar sus pesos: oficial, blue o liqui (Ingrese ESC para salir del programa)"));
-let pesos = (prompt("Ingrese, sin el simbolo $, la cantidad de pesos a cambiar"));
+let tipoDolar = prompt(`Ingrese el tipo de dolar al que quiere cambiar sus pesos: ${nombres.join(" - ")} (Ingrese ESC para salir del programa)`);
+let pesos = prompt("Ingrese, sin el simbolo $, la cantidad de dolares a cambiar");
 
 while(tipoDolar != "ESC"){
-    alert(`$${pesos} son $${dolar(pesos, tipoDolar)} USD al cambio ${tipoDolar}`);
-    tipoDolar = prompt("Ingrese el tipo de dolar al que quiere cambiar sus pesos: oficial, blue o liqui (Ingrese ESC para salir del programa)");
-    pesos = prompt("Ingrese, sin el simbolo $, la cantidad de pesos a cambiar");
+    alert(`$${pesos} USD son $${dolar(pesos, tipoDolar)} ARS al cambio ${tipoDolar}`);
+    tipoDolar = prompt(`Ingrese el tipo de dolar al que quiere cambiar sus pesos: ${nombres.join(" - ")} (Ingrese ESC para salir del programa)`);
+    pesos = prompt("Ingrese, sin el simbolo $, la cantidad de dolares a cambiar");
 };
