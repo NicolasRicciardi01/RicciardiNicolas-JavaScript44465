d42e0bc0f8172367be97772a9873d1031fd15a3a
Nicolas <nicolasricciardi24@gmail.com>
2022-10-06 22:50:14 -0300

    Jons 2da entrega

--- index.js
+++ index.js
@@ -1,58 +1,49 @@
-const nombres = ["oficial", "blue", "liqui"];
-const precios = [139, 270, 280];
+const divisas = `[
+    {
+        "nombre": "ARS",
+        "precio": 1
+    },
+    {
+        "nombre": "USDoficial",
+        "precio": 139
+    },
+    {
+        "nombre": "USDblue",
+        "precio": 270
+    },
+    {
+        "nombre": "USDliqui",
+        "precio": 280
+    }
+]`
 
-function tdolar(nombre, precio){
-    this.nombre = nombre
-    this.precio = precio
+const jsonDivisas = JSON.parse(divisas)
+
+// Variables
+const tipoMoneda1 = document.getElementById("moneda1");
+const cantidadMoneda1 = document.getElementById("cantidad1");
+const tipoMoneda2 = document.getElementById("moneda2");
+const cantidadMoneda2 = document.getElementById("cantidad2");
+
+const rateEl = document.getElementById("rate");
+const cambio = document.getElementById("cambio");
+
+// Calculadora
+function calcular(){
+    let moneda_1 = jsonDivisas.find(elemento => {return elemento.nombre === tipoMoneda1.value});
+    let moneda_2 = jsonDivisas.find(elemento => {return elemento.nombre === tipoMoneda2.value});
+
+    rateEl.innerText = `1 ${moneda_1.nombre} = ${moneda_2.precio} ${moneda_2.nombre}`;  
+
+    if (moneda_1.nombre === "ARS"){
+        cantidadMoneda2.value = (cantidadMoneda1.value / moneda_2.precio).toFixed(2);
+    }  else {
+        cantidadMoneda2.value = (cantidadMoneda1.value * moneda_1.precio).toFixed(2);
+    }
 };
 
-const oficial = new tdolar(nombres[0], precios[0]);
-const blue = new tdolar(nombres[1], precios[1]);
-const liqui = new tdolar(nombres[2], precios[2]);
-
-/* Funcion para calcular los dolares */
-function dolar(numero, typeDollar, voF){
-    if(voF){
-        switch(typeDollar){
-            case oficial.nombre:
-                return numero * oficial.precio;
-            case blue.nombre:
-                return numero * blue.precio;
-            case liqui.nombre:
-                return numero * liqui.precio;
-            default :
-                console.log("Tipo de dolar no identificado");
-                return "ERROR DOLAR NO IDENTIFICADO";
-        };
-    }else{
-        console.log("voF: falso");
-        alert("ERROR NOMBRE DOLAR");
-    };
-};
-
-function nameVerif(dNombre){
-    console.log(dNombre === "oficial" || dNombre === "blue" || dNombre === "liqui")
-    return dNombre === "oficial" || dNombre === "blue" || dNombre === "liqui";
-};
-
-let tipoDolar = prompt(`Ingrese el tipo de dolar al que quiere cambiar sus pesos: ${nombres.join(" - ")} (Ingrese ESC para salir del programa)`);
-let pesos = prompt("Ingrese, sin el simbolo $, la cantidad de dolares a cambiar");
-let uHtml = document.getElementById("usdHtml").innerHTML;
-const miBtn = document.getElementById("reinicio");
-
-    while(tipoDolar != "ESC"){
-        document.getElementById("pesosHtml").innerHTML = `Pesos: $${pesos}`;  
-        alert(`$${pesos} USD son $${dolar(pesos, tipoDolar, nameVerif(tipoDolar))} ARS al cambio ${tipoDolar}`);
-        document.getElementById("usdHtml").innerHTML = `Usd: $${dolar(pesos, tipoDolar, nameVerif(tipoDolar))}`;
-        tipoDolar = prompt(`Ingrese el tipo de dolar al que quiere cambiar sus pesos: ${nombres.join(" - ")} (Ingrese ESC para salir del programa)`);
-        pesos = prompt("Ingrese, sin el simbolo $, la cantidad de dolares a cambiar");
-    };
-
-miBtn.addEventListener("click", function(e) {
-    console.log("El boton fue clickeado");
-    const nodoTexto = document.createTextNode(`Los precios de los dolares son: ${oficial.nombre} = $${oficial.precio}, 
-                                                                                ${blue.nombre} = $${blue.precio},
-                                                                                y ${liqui.nombre} = $${liqui.precio}.`);
-    document.body.appendChild(nodoTexto);
-
-});
+// Event listeners de inputs del usuario
+tipoMoneda1.addEventListener("change", calcular);
+tipoMoneda2.addEventListener("change", calcular);
+cantidadMoneda1.addEventListener("input", calcular);
+cantidadMoneda2.addEventListener("input", calcular);
