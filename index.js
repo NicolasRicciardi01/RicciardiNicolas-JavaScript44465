88b36fddafb390bf1f29f2da438011875c99aa01
Nicolas <nicolasricciardi24@gmail.com>
2022-10-11 23:40:22 -0300

    Desafio optimizando

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
+// Lista de divisas a usar con su respectivo precio
 const divisas = `[
     {
         "nombre": "ARS",
-        "precio": 1
+        "precio": 0.0071
     },
     {
         "nombre": "USDoficial",
-        "precio": 139
+        "precio": 1
     },
     {
         "nombre": "USDblue",
-        "precio": 270
+        "precio": 1.94
     },
     {
         "nombre": "USDliqui",
-        "precio": 280
+        "precio": 2.01
     }
 ]`
 
-const jsonDivisas = JSON.parse(divisas)
+// Lista de devisas como variable
+const jsonDivisas = JSON.parse(divisas);
 
 // Variables
 const tipoMoneda1 = document.getElementById("moneda1");
@@ -25,8 +27,15 @@ const cantidadMoneda1 = document.getElementById("cantidad1");
 const tipoMoneda2 = document.getElementById("moneda2");
 const cantidadMoneda2 = document.getElementById("cantidad2");
 
+// Variables boton
 const rateEl = document.getElementById("rate");
 const cambio = document.getElementById("cambio");
+
+// Event listeners de imputs del usuario
+tipoMoneda1.addEventListener("change", calcular);
+tipoMoneda2.addEventListener("change", calcular);
+cantidadMoneda1.addEventListener("input", calcular);
+cantidadMoneda2.addEventListener("input", calcular);
 
 // Calculadora
 function calcular(){
@@ -35,15 +44,17 @@ function calcular(){
 
     rateEl.innerText = `1 ${moneda_1.nombre} = ${moneda_2.precio} ${moneda_2.nombre}`;  
 
-    if (moneda_1.nombre === "ARS"){
-        cantidadMoneda2.value = (cantidadMoneda1.value / moneda_2.precio).toFixed(2);
-    }  else {
-        cantidadMoneda2.value = (cantidadMoneda1.value * moneda_1.precio).toFixed(2);
+    if (moneda_1.nombre === "ARS" && moneda_2.nombre === "USDoficial"){
+        cantidadMoneda2.value = (cantidadMoneda1.value / 139).toFixed(2);
+    } else {
+        cantidadMoneda2.value = (cantidadMoneda1.value * moneda_2.precio).toFixed(2);
     }
-};
+}
 
-// Event listeners de inputs del usuario
-tipoMoneda1.addEventListener("change", calcular);
-tipoMoneda2.addEventListener("change", calcular);
-cantidadMoneda1.addEventListener("input", calcular);
-cantidadMoneda2.addEventListener("input", calcular);
+// Boton de intercambio 
+cambio.addEventListener("click", () =>{
+    let temp = tipoMoneda1.value;
+    tipoMoneda1.value = tipoMoneda2.value;
+    tipoMoneda2.value = temp;
+    calcular();
+})
