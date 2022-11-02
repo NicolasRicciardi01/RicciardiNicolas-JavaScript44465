89848b02b42caa1f01bec1b2805b46d964b99ce3
Nicolas <nicolasricciardi24@gmail.com>
2022-11-02 00:48:20 -0300

    Final

--- index.js
+++ index.js
@@ -1,38 +1,3 @@
-/* fetch("https://api.bluelytics.com.ar/v2/latest")
-    .then(res => res.json())
-    .then(data => console.log(data))
-    .catch(error => console.log("ERROR")) */
-
-fetch("./JSON/divisas.json")
-    .then(res => res.json())
-    .then(data => console.log(data));
-
-
-// Lista de divisas a usar con su respectivo precio
-const divisas = `[
-    {
-        "nombre": "ARS",
-        "precio": 1
-    },
-    {
-        "nombre": "USDoficial",
-        "precio": 154           
-    },
-    {
-        "nombre": "USDblue",
-        "precio": 288
-    },
-    {
-        "nombre": "USDliqui",
-        "precio": 295
-    }
-]`
-
-// Lista de devisas como variable array
-const stringJson = JSON.stringify(fetch("./JSON/divisas.json"));
-console.log(stringJson);
-const jsonDivisas = JSON.parse(divisas);
-
 // Variables
 const tipoMoneda1 = document.getElementById("moneda1");
 const cantidadMoneda1 = document.getElementById("cantidad1");
@@ -50,17 +15,16 @@ cantidadMoneda1.addEventListener("input", calcular);
 cantidadMoneda2.addEventListener("input", calcular);
 
 // Calculadora
-function calcular(){
-    let moneda_1 = jsonDivisas.find(elemento => {return elemento.nombre === tipoMoneda1.value});
-    let moneda_2 = jsonDivisas.find(elemento => {return elemento.nombre === tipoMoneda2.value});
-
-    rateEl.innerText = `1 ${moneda_1.nombre} = ${moneda_2.precio} ${moneda_2.nombre}`;  
-
-    if (moneda_1.nombre != "ARS"){
-        cantidadMoneda2.value = ((cantidadMoneda1.value * moneda_1.precio) / moneda_2.precio).toFixed(2);
-    } else {
-        cantidadMoneda2.value = (cantidadMoneda1.value * moneda_2.precio).toFixed(2);
-    }
+function calcular() {
+    const moneda_1 = tipoMoneda1.value;
+    const moneda_2 = tipoMoneda2.value;
+    fetch(`https://v6.exchangerate-api.com/v6/b71ba5a6f69833fe3ac900ec/latest/${moneda_1}`)
+        .then((res) => res.json())
+        .then((data) => {
+        const rate = data.conversion_rates[moneda_2];
+        rateEl.innerText = `1 ${moneda_1} = ${rate} ${moneda_2}`;
+        cantidadMoneda2.value = (cantidadMoneda1.value * rate).toFixed(2);
+    });
 }
 
 // Boton de intercambio 
@@ -70,7 +34,7 @@ cambio.addEventListener("click", () =>{
     tipoMoneda2.value = temp;
     calcular();
 
-    // Alerta
+// Alerta
     Swal.fire({
         title: 'Hecho!',
         text: 'Monedas intercambiadas',
