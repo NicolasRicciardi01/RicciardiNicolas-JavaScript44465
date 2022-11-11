4731eab2d2fbc0f8a284c732bcb8b7b31ce5404c
Nicolas <nicolasricciardi24@gmail.com>
2022-11-11 11:17:52 -0300

    Entrega final 2

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const tipoMoneda1 = document.getElementById("moneda1");
 const cantidadMoneda1 = document.getElementById("cantidad1");
 const tipoMoneda2 = document.getElementById("moneda2");
 const cantidadMoneda2 = document.getElementById("cantidad2");
+const historial = document.getElementById("historialId");
 
 // Variables boton
 const rateEl = document.getElementById("rate");
@@ -14,17 +15,33 @@ tipoMoneda2.addEventListener("change", calcular);
 cantidadMoneda1.addEventListener("input", calcular);
 cantidadMoneda2.addEventListener("input", calcular);
 
+// Historial
+for(let i = 0; i < localStorage.length; i++){
+    let clave = localStorage.key(i);
+    let tag = document.createElement("p");
+    let text = document.createTextNode(`${localStorage.getItem(clave)}`);
+    tag.appendChild(text);
+    historial.appendChild(tag);
+}
+
 // Calculadora
 function calcular() {
     const moneda_1 = tipoMoneda1.value;
     const moneda_2 = tipoMoneda2.value;
-    fetch(`https://v6.exchangerate-api.com/v6/b71ba5a6f69833fe3ac900ec/latest/${moneda_1}`)
+    fetch(`https://v6.exchangerate-api.com/v6/06283b96790043ee5c5264d9/latest/${moneda_1}`)
         .then((res) => res.json())
         .then((data) => {
         const rate = data.conversion_rates[moneda_2];
         rateEl.innerText = `1 ${moneda_1} = ${rate} ${moneda_2}`;
         cantidadMoneda2.value = (cantidadMoneda1.value * rate).toFixed(2);
+        let dia = new Date();
+        localStorage.setItem(`${dia}`, `${moneda_1}: ${cantidadMoneda1.value} => ${moneda_2} ${cantidadMoneda2.value}`);
+        let tag = document.createElement("p");
+        let text = document.createTextNode(`${cantidadMoneda1.value} => ${moneda_2} ${cantidadMoneda2.value}`);
+        tag.appendChild(text);
+        historial.appendChild(tag);
     });
+
 }
 
 // Boton de intercambio 
